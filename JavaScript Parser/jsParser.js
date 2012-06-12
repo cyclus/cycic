7b19f012f62506d814b9ff109824c9edddb19589
Thinh Lam <thinh.lam@utexas.edu>
2012-06-12 01:13:46 -0500

    Redesign parser to recursively parse. Makes code a bit cleaner and less cluttered.

--- JavaScript Parser/jsParser.js
+++ JavaScript Parser/jsParser.js
@@ -11,70 +11,91 @@ xhttp.send();
 xmlDoc=xhttp.responseXML; 
 x = xmlDoc.childNodes[0].childNodes;
 
-
+//Print main parent name
 document.write(xmlDoc.getElementsByTagName('element')[0].getAttribute('name') + ': {<br>');
-var elements = xmlDoc.childNodes[0].childNodes[1].childNodes[1].childNodes
-for (j=0; j<xmlDoc.childNodes[0].childNodes[1].childNodes[1].childNodes.length; j++){
-		if(j%2){
-			if(elements[j].getAttribute('name') === null){
-				document.write('&nbsp&nbsp&nbsp&nbsp'+elements[j].nodeName+': {<br>');
-			}
-			else{
-				document.write('&nbsp&nbsp&nbsp&nbsp'+elements[j].getAttribute('name'));
-				if(elements[j].nodeName == 'ref'){
-					document.write('(ref)');
-				}
-				document.write(': {<br>');
-				=
-			}
-			for(k=0; k<elements[j].childNodes.length; k++){
-				if(k%2){
-					if(elements[j].childNodes[k].getAttribute('name')){
-						document.write('&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+'\"name\": '+elements[j].childNodes[k].getAttribute('name'));
-					}
-					else if(elements[j].childNodes[k].getAttribute('type')){
-						document.write('&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+'\"type\": '+elements[j].childNodes[k].getAttribute('type'));
-					}
-					
-					if(elements[j].childNodes[k].getAttribute('name') === null){
-						if(elements[j].childNodes[k].nodeName == 'ref'){
-							document.write('(ref)');
-						}
-						else if(elements[j].childNodes[k].nodeName != 'data'){
-							document.write('&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+elements[j].childNodes[k].nodeName);
-						}
-					}
-					document.write(': {<br>');
-				}
-				for(i=0; i<elements[j].childNodes[k].childNodes.length;i++){
-					if(i%2){
-						document.write('&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+
-							'\"name\": '+elements[j].childNodes[k].childNodes[i].getAttribute('name'));
-						if(elements[j].childNodes[k].childNodes[i].nodeName == 'ref'){
-							document.write('(ref)');
-						}
-						document.write('<br>');
-					}
-					for(a=0; a<elements[j].childNodes[k].childNodes[i].childNodes.length;a++){
-						if(a%2){
-							if(elements[j].childNodes[k].childNodes[i].childNodes[a].getAttribute('name')){
-								document.write('&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+'\"name\": '+elements[j].childNodes[k].childNodes[i].childNodes[a].getAttribute('name'));
-							}
-							if(elements[j].childNodes[k].childNodes[i].childNodes[a].getAttribute('type')){
-								document.write('&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp'+'\"type\": '+elements[j].childNodes[k].childNodes[i].childNodes[a].getAttribute('type'));
-							}
-							if(elements[j].childNodes[k].childNodes[i].childNodes[a].nodeName == 'ref'){
-								document.write('(ref)');
-							}
-							document.write('<br>');
-						}
-					}
-				}
-			}
-			document.write('&nbsp&nbsp&nbsp&nbsp'+'&nbsp&nbsp&nbsp&nbsp }<br>'+'&nbsp&nbsp&nbsp&nbsp }<br>');
+
+var elements = x[1].childNodes[1].childNodes
+var space = '';
+var node = new Object();
+var list = new Array();
+
+//Print main element names
+for(j=0; j<elements.length; j++){
+	if(j%2){
+		if(elements[j].getAttribute('name') === null){
+			document.write('&nbsp&nbsp&nbsp&nbsp'+elements[j].nodeName+': {<br>');
 		}
+		else{
+			document.write('&nbsp&nbsp&nbsp&nbsp'+elements[j].getAttribute('name') + ': {<br>');
+		}
+		//Under each element name, print out all of its possible children
+		printChildren(elements[j].childNodes, 2);
+		document.write('&nbsp&nbsp&nbsp&nbsp}<br>');
+	}
 }
-document.write('}<br>');
+
+var k=0;
+function printChildren(a, spaceNumber){
+	for (i=0; i<a.length; i++){
+		//Extract only the element, not the text objects
+		if(i%2){
+			//Input appropriate amount of spaces to format output
+			inputSpaces(spaceNumber);
+			
+			//If the child is a tag, print out just its children since it has no 'name' or 'type' attribute
+			if(a[i].getAttribute('name') === null){
+				if(a[i].getAttribute('type') === null){
+					document.write(a[i].nodeName+': {<br>');
+					printChildren(a[i].childNodes, spaceNumber + 1);
+				}
+			}
+			
+			//If the object has a 'name' attribute, print it
+			if(a[i].getAttribute('name')){
+				document.write('name: '+ a[i].getAttribute('name') +'<br>');
+			}
+			
+			//If the object has a 'type' attribute, print it
+			if(a[i].getAttribute('type')){
+				document.write('type: '+a[i].getAttribute('type') +'<br>');
+			}
+			
+			//Check for its children's children. If it exists, recursively call printChildren
+			if(hasChildren(a[i])){
+				printChildren(a[i].childNodes, spaceNumber + 1);
+			}
+			
+		}
+		
+	}
+	
+	
+}
+
+//Helper function used to determine if a node has children or not
+function hasChildren(node){
+	if(node.childNodes.length >= 1){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+//Helper function used to print out 'num' amount of tabs for formatting purposes
+function  inputSpaces(num){
+	for(k=0; k < num; k ++){
+		document.write('&nbsp&nbsp&nbsp&nbsp');
+	}
+}
+
+
+//																																\\
+//																																\\
+//MENTAL NOTES TO SELF																											\\
+//																																\\
+//																																\\
+
 //xmlDoc.childNodes[0].nodeName -- 'grammer'
 //xmlDoc.childNodes[0].childNodes[1].nodeName -- 'define'
 //xmlDoc.childNodes[0].childNodes[1].childNodes[1] --element RecipeReactor 
