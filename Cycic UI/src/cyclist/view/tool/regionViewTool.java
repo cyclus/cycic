e177b2c46f846565786c97f602e00d109333b1a4
flanflanagan <flanny65@gmail.com>
2013-07-17 13:08:33 -0500

    Test

---
+++ Cycic UI/src/cyclist/view/tool/regionViewTool.java
@@ -0,0 +1,35 @@
+package cyclist.view.tool;
+
+import javafx.scene.image.Image;
+
+import org.puremvc.java.multicore.patterns.mediator.Mediator;
+
+import cyclist.view.component.View;
+import cyclist.view.tool.mediator.regionViewMediator;
+import cyclist.view.tool.view.regionView;
+
+public class regionViewTool implements Tool {
+
+	@Override
+	public Image getIcon() {
+		return Resources.getIcon("table");
+	}
+
+	@Override
+	public String getName() {
+		return "Region Form";
+	}
+
+	@Override
+	public View getView() {
+		View view = new regionView();
+		view.setParam("Region Form");
+		return view;
+	}
+	
+	@Override
+	public Mediator getMediator() {
+		return new regionViewMediator();
+	}
+
+}
